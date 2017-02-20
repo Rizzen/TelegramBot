ade98150d6ee6eab5a98eb76ccb492c4b9b40e71
Vlad <vladskryto@gmail.com>
2017-02-20 23:12:51 +0400

    install Json and add to class

--- TelegramBot/SimpleBot.cs
+++ TelegramBot/SimpleBot.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-// подключи библу json
+using Newtonsoft.Json;
 namespace TelegramBot
 {
     class SimpleBot
