57390f439057ab2d63ff0f0971cacbef4b89d0c0
Azcinor <Azcinor@gmail.com>
2017-02-20 21:36:40 +0200

    refactor

--- TelegramBot/Program.cs
+++ TelegramBot/Program.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TelegramBot
+﻿namespace TelegramBot
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //test pull comment
+            new SimpleBot();
         }
     }
 }
--- TelegramBot/SimpleBot.cs
+++ TelegramBot/SimpleBot.cs
@@ -1,18 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Newtonsoft.Json;
-using System.Net;
-using System.Net.Sockets;
-
-
-namespace TelegramBot
+﻿namespace TelegramBot
 {
     class SimpleBot
     {
-        static string Token = @"375416144:AAHDLsJ_0MOow-u_LbwdWqRvfB4uyRByryQ";
+        static string TOKEN = @"375416144:AAHDLsJ_0MOow-u_LbwdWqRvfB4uyRByryQ";
+
         void GetUpdates()
         {
 
