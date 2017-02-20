2760b1cf51faa8a6c9086befa00673e5265e48a5
Rizzen <onemangd@gmail.com>
2017-02-20 23:30:53 +0400

    Added User class

--- TelegramBot/API Classes/User.cs
+++ TelegramBot/API Classes/User.cs
@@ -10,7 +10,7 @@ namespace TelegramBot.API_Classes
     {
         //Unique identifier for this user or bot
         public int Id { get; set; }
-        //	User‘s or bot’s first name
+        //User‘s or bot’s first name
         public string FirstName { get; set; }
         //Optional. User‘s or bot’s last name
         public string LastName { get; set; }
