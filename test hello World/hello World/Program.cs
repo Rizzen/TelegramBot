96880b122828af2e62caa0237a92134a7ff148ae
Marcus Weaver <onemangd@gmail.com>
2017-02-20 20:54:56 +0300

    Add files via upload

---
+++ test hello World/hello World/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello WOrld");
+        }
+    }
+}
