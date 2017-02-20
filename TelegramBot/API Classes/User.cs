ea3e67cb40b8ff48765d0217e6749a491b58feb0
Rizzen <onemangd@gmail.com>
2017-02-20 23:38:42 +0400

    Edited User

--- TelegramBot/API Classes/User.cs
+++ TelegramBot/API Classes/User.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TelegramBot.API_Classes
+﻿namespace TelegramBot.API_Classes
 {
-    class User
+    public class User
     {
         //Unique identifier for this user or bot
         public int Id { get; set; }
@@ -17,20 +11,5 @@ namespace TelegramBot.API_Classes
         //Optional. User‘s or bot’s username
         public string Username { get; set; }
 
-        public User() { }
-
-        public User(int _id, string _firstName)
-        {
-            Id = _id;
-            FirstName = _firstName;
-        }
-
-        public User(int _id, string _firstName, string _lastName,string _username)
-        {
-            Id = _id;
-            FirstName = _firstName;
-            LastName = _lastName;
-            Username = _username;
-        }
     }
 }
