5bf9ae572be078aa20f875e565d6a941f562f3f6
Rizzen <onemangd@gmail.com>
2017-02-21 00:04:23 +0400

    Added Chat

---
+++ TelegramBot/API Classes/Chat.cs
@@ -0,0 +1,24 @@
+﻿namespace TelegramBot.API_Classes
+{
+    class Chat
+    {
+        //Unique identifier for this chat. 
+        //This number may be greater than 32 bits and some programming languages may have difficulty/silent defects in interpreting it. 
+        //But it smaller than 52 bits, so a signed 64 bit integer or double-precision float type are safe for storing this identifier.
+        public int Id { get; set; }
+        //Type of chat, can be either “private”, “group”, “supergroup” or “channel”
+        public string Type { get; set; }
+        //Optional. Title, for supergroups, channels and group chats
+        public string Title { get; set; }
+        //	Optional. Username, for private chats, supergroups and channels if available
+        public string Username { get; set; }
+        //Optional. First name of the other party in a private chat
+        public string FirstName { get; set; }
+        //Optional. Last name of the other party in a private chat
+        public string LastName { get; set; }
+        //Optional. True if a group has ‘All Members Are Admins’ enabled.
+        public bool AllMembersAreAdministrators { get; set; }
+
+
+    }
+}
--- TelegramBot/API Classes/Message.cs
+++ TelegramBot/API Classes/Message.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TelegramBot.API_Classes
+﻿namespace TelegramBot.API_Classes
 {
     class Message
     {
+
     }
 }
