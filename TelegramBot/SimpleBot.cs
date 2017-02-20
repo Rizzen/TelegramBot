6fd3171cc0bd115f7a73178b32393158bbd23698
Vlad <vladskryto@gmail.com>
2017-02-20 23:33:26 +0400

    Added libraries and void GetUpdate()

--- TelegramBot/SimpleBot.cs
+++ TelegramBot/SimpleBot.cs
@@ -4,10 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using System.Net;
+using System.Net.Sockets;
+
+
 namespace TelegramBot
 {
     class SimpleBot
     {
+        static string Token = @"375416144:AAHDLsJ_0MOow-u_LbwdWqRvfB4uyRByryQ";
+        void GetUpdates()
+        {
 
+        }
     }
 }
