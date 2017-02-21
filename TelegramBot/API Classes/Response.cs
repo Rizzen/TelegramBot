04b7a8ccb7ebed8186ce08f84a789b3c85dc710f
Azcinor <Azcinor@gmail.com>
2017-02-22 00:20:03 +0200

    Response class

---
+++ TelegramBot/API Classes/Response.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace TelegramBot.API_Classes
+{
+    internal class Response
+    {
+        [JsonProperty("ok")]
+        public bool Success { get; set; }
+
+        [JsonProperty("result")]
+        public Update[] Updates { get; set; }
+    }
+}
