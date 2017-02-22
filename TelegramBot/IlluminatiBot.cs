bc6defeddd3ec2a1ab68c463f32d9dce4a7e0468
Rizzen <onemangd@gmail.com>
2017-02-22 18:21:19 +0400

    illuminati

---
+++ TelegramBot/IlluminatiBot.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramBot
+{
+    class IlluminatiBot
+    {
+        const string TOKEN = @"375416144:AAHDLsJ_0MOow-u_LbwdWqRvfB4uyRByryQ";
+        const string URI = @"https://api.telegram.org/bot";
+    }
+}
