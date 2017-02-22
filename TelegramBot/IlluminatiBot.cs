5eabf1db082d153aef1bfd476f8d671fdab418ad
Rizzen <onemangd@gmail.com>
2017-02-22 18:43:55 +0400

    illum

--- TelegramBot/IlluminatiBot.cs
+++ TelegramBot/IlluminatiBot.cs
@@ -1,14 +1,82 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using TelegramBot.API_Classes;
+using System.Threading;
 
 namespace TelegramBot
 {
     class IlluminatiBot
     {
-        const string TOKEN = @"375416144:AAHDLsJ_0MOow-u_LbwdWqRvfB4uyRByryQ";
+        const string TOKEN = @"334103433:AAEc9KxmTjbTyzjK2DFpcuWUeUwQIPBKOi8";
         const string URI = @"https://api.telegram.org/bot";
+
+        private int _updateID = 0;
+
+
+        public IlluminatiBot()
+        {
+            GetUpdates(); 
+        }
+
+        void GetUpdates()
+        {
+            Console.WriteLine($"Обновление: {_updateID}");
+            var req = (HttpWebRequest)WebRequest.Create(URI + TOKEN + "/getUpdates" + "?offset=" + (_updateID + 1));
+            var resp = (HttpWebResponse)req.GetResponse();
+
+            using (var stream = resp.GetResponseStream())
+            {
+                using (var sReader = new StreamReader(stream))
+                {
+                    string responsedJson = sReader.ReadToEnd();
+                    sReader.Close();
+
+                    // Пытаемся загрузить все, что ему прислали
+                    try
+                    {
+                        var currentUpdate = Newtonsoft.Json.JsonConvert.DeserializeObject<Response>(responsedJson);
+                        DownloadAll(currentUpdate);
+                        ////
+                        // _updateID = update.UpdateId + 1; — пока пусть будет закоменчено, чтобы не очищать эвенты
+                        // здесь будем обрабатывать или класть в очередь
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Fail");
+                    }
+
+                }
+            }
+
+            Console.ReadLine();
+        }
+
+
+        void DownloadAll(Response botResponcse)
+        {
+            int i = 0;
+            foreach (var update in botResponcse.Updates)
+            {
+                _updateID = update.UpdateId;
+                Console.WriteLine(update.Message.Text);
+                try
+                {
+                    using (WebResponse responce = WebRequest.Create(update.Message.Text).GetResponse())
+                    {
+                        // string format = responce.Headers.GetValues("Content-Type")[0].Split(new char[] { '/' }).Last();
+                        string format = Path.GetExtension(update.Message.Text);
+                        Console.WriteLine("Format: " + format);
+                        var wClient = new WebClient();
+                        wClient.DownloadFile(update.Message.Text, "File" + i + "." + format);
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("DownloadFailed");
+                }
+                i++;
+            }
+        }
     }
 }
--- TelegramBot/Program.cs
+++ TelegramBot/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            new SimpleBot();
+            new IlluminatiBot();
         }
     }
 }
