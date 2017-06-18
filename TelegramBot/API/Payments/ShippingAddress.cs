4573d1b50fbfb35b05403eece21bfa86ced28d80
Marcus Weaver <onemangd@gmail.com>
2017-06-18 14:50:59 +0300

    payments

---
+++ TelegramBot/API/Payments/ShippingAddress.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+
+namespace TelegramBot.API.Payments
+{
+    class ShippingAddress
+    {
+        /// <summary>
+        /// ISO 3166-1 alpha-2 country code
+        /// </summary>
+        [JsonProperty("country_code")]
+        internal string CountryCode { get; set; }
+
+        /// <summary>
+        /// State, if applicable
+        /// </summary>
+        [JsonProperty("state")]
+        internal string State { get; set; }
+
+        /// <summary>
+        /// City
+        /// </summary>
+        [JsonProperty("city")]
+        internal string City { get; set; }
+
+        /// <summary>
+        /// First line for the address
+        /// </summary>
+        [JsonProperty("street_line1")]
+        internal string StreetLine1 { get; set; }
+
+        /// <summary>
+        /// Second line for the address
+        /// </summary>
+        [JsonProperty("street_line2")]
+        internal string StreetLine2 { get; set; }
+
+        /// <summary>
+        /// Address post code
+        /// </summary>
+        [JsonProperty("post_code")]
+        internal string PostCode { get; set; }
+    }
+}
---
+++ TelegramBot/API/Payments/SuccessfulPayment.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+
+namespace TelegramBot.API.Payments
+{
+    public class SuccessfulPayment
+    {
+        /// <summary>
+        /// Three-letter ISO 4217 currency code
+        /// </summary>
+        [JsonProperty("currency")]
+        internal string Currency { get; set; }
+
+        /// <summary>
+        /// Total price in the smallest units of the currency (integer, not float/double). 
+        /// For example, for a price of US$ 1.45 pass amount = 145. 
+        /// See the exp parameter in currencies.json, it shows the number of digits past the decimal point for each currency (2 for the majority of currencies).
+        /// </summary>
+        [JsonProperty("total_amount")]
+        internal int TotalAmount { get; set; }
+
+        /// <summary>
+        /// Bot specified invoice payload
+        /// </summary>
+        [JsonProperty("invoice_payload")]
+        internal string InvoicePayload { get; set; }
+
+        /// <summary>
+        /// Optional. Identifier of the shipping option chosen by the user
+        /// </summary>
+        [JsonProperty("shipping_option_id")]
+        internal string ShippingOptionId { get; set; }
+
+        [JsonProperty("order_info")]
+        internal OrderInfo OrderInfo { get; set; }
+
+        /// <summary>
+        /// Telegram payment identifier
+        /// </summary>
+        [JsonProperty("telegram_payment_charge_id")]
+        internal string TelegramPaymentChargeId { get; set; }
+
+        /// <summary>
+        /// Provider payment identifier
+        /// </summary>
+        [JsonProperty("provider_payment_charge_id")]
+        internal string ProviderPaymentCargeId { get; set; }
+    }
+}
